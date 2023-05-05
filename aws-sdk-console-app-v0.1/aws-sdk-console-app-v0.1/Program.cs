1247bf743bf7ea4f4cd554a6952e3c8e70b8d4af
Katate <pfvaz96@gmail.com>
2023-05-05 17:02:19 +0100

    First commit, created dotnet core console app

---
+++ aws-sdk-console-app-v0.1/aws-sdk-console-app-v0.1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace aws_sdk_console_app_v0._1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
